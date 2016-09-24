dc79500020bf87c9a1aefb512426b3ce469a613a
Korirmitchelle <korirmitchelle@gmail.com>
2016-09-24 16:21:14 +0300

    signup

--- app/src/main/java/com/example/mitchelle/somasoma/CoolText.java
+++ app/src/main/java/com/example/mitchelle/somasoma/CoolText.java
@@ -4,6 +4,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CoolText extends AppCompatActivity {
 
@@ -13,8 +15,6 @@ public class CoolText extends AppCompatActivity {
         setContentView(R.layout.activity_cool_text);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
         /*start*/
@@ -27,5 +27,27 @@ public class CoolText extends AppCompatActivity {
 
         writer.setCharacterDelay(150);
         writer.animateText("We give you the best gift ideas Ever");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
