8577ed1b947aee02772367ce112b9f659a2a605a
AndrewKahora <cahndrew@gmail.com>
2016-09-26 16:02:36 +0300

    adding activity 2..

--- app/src/main/java/com/example/andrew/zawadi/Activity2.java
+++ app/src/main/java/com/example/andrew/zawadi/Activity2.java
@@ -12,6 +12,6 @@ public class Activity2 extends SwipeBackActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        setDragEdge(SwipeBackLayout.DragEdge.LEFT);
+        setDragEdge(SwipeBackLayout.DragEdge.RIGHT);
     }
 }
