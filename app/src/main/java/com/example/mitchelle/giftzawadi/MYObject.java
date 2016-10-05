3f510459e1396d20fc10bebced970a905c4a00c4
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-05 11:52:12 +0300

    models class

--- app/src/main/java/com/example/mitchelle/giftzawadi/MYObject.java
+++ app/src/main/java/com/example/mitchelle/giftzawadi/MYObject.java
@@ -4,4 +4,27 @@ package com.example.mitchelle.giftzawadi;
  * Created by mitchelle on 10/5/16.
  */
 public class MYObject {
+    private String name;
+    private int photo;
+
+    public MYObject(String name, int photo) {
+        this.name = name;
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
 }
