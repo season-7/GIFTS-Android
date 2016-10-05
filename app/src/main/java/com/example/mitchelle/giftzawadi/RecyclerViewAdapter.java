f139e351e70c846510ef7f0a7562c9ca8248a393
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-05 11:52:56 +0300

    Adapter to inflate views

--- app/src/main/java/com/example/mitchelle/giftzawadi/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mitchelle/giftzawadi/RecyclerViewAdapter.java
@@ -1,7 +1,43 @@
 package com.example.mitchelle.giftzawadi;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
 /**
  * Created by mitchelle on 10/5/16.
  */
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+    private List<MYObject> itemList;
+    private Context context;
+
+    public RecyclerViewAdapter(Context context, List<MYObject> itemList) {
+        this.itemList = itemList;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, null);
+        RecyclerViewHolder rcv = new RecyclerViewHolder(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.manPreference.setText(itemList.get(position).getName());
+        holder.background.setImageResource(itemList.get(position).getPhoto());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.itemList.size();
+    }
 }
