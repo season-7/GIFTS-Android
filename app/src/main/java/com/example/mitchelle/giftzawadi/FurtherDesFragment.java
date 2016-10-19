4c4ef62290f16105f539066f2e520745a2157d9c
Korirmitchelle <korirmitchelle@gmail.com>
2016-10-19 17:41:17 +0300

    description fragment before displaying database data

--- app/src/main/java/com/example/mitchelle/giftzawadi/FurtherDesFragment.java
+++ app/src/main/java/com/example/mitchelle/giftzawadi/FurtherDesFragment.java
@@ -1,101 +1,155 @@
 package com.example.mitchelle.giftzawadi;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class FurtherDesFragment extends ListFragment  {
+    private ImageView movetofinal;
 
 
-/
-public class FurtherDesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //// For foodie
+    String[] foods = new String[] {
+            "Foodie",
+            "Arts Person",
+            "Fashionista",
+            "Gadgets Person",
+            "Books Reader",
+            "Movie Freak",
+            "Event Person",
+            "Sporty Person"
+    };
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    int[] foodimages = new int[]{
+            R.drawable.foodiepeople,
+            R.drawable.artsperson,
+            R.drawable.fashion,
+            R.drawable.gadgetpeople,
+            R.drawable.bookmanfinal,
+            R.drawable.newmovies,
+            R.drawable.eventsperson,
+            R.drawable.finalactiveman
 
-    private OnFragmentInteractionListener mListener;
 
-    public FurtherDesFragment() {
-        // Required empty public constructor
-    }
+    };
+    /////end foodie
+    ////////
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FurtherDesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FurtherDesFragment newInstance(String param1, String param2) {
-        FurtherDesFragment fragment = new FurtherDesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // Array of strings to store currencies
+    String[] currency = new String[]{
+            "Indian Rupee",
+            "Pakistani Rupee",
+            "Sri Lankan Rupee",
+            "Renminbi",
+            "Bangladeshi Taka",
+            "Nepalese Rupee",
+            "Afghani",
+            "North Korean Won",
+            "South Korean Won",
+            "Japanese Yen"
+    };
+    public String cat;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+             cat = bundle.getString("category", "nothing");
+            String desc = bundle.getString("desc", "nothing");
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_further_des, container, false);
-    }
+        if(cat.equals("Foodie")){
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            // Each row in the list stores country name, currency and flag
+            List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+
+            for(int i=0;i<9;i++){
+                try {
+                    HashMap<String, String> hm = new HashMap<String,String>();
+                    hm.put("txt", " " + foods[i]);
+                    hm.put("cur"," " + currency[i]);
+                    hm.put("flag", Integer.toString(foodimages[i]) );
+                    aList.add(hm);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Keys used in Hashmap
+            String[] from = { "flag","txt"};
+
+            // Ids of views in listview_layout
+            int[] to = { R.id.flag,R.id.txt};
+
+            // Instantiating an adapter to store each items
+            // R.layout.listview_layout defines the layout of each item
+
+            SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_layout, from, to);
+
+            setListAdapter(adapter);
+            getActivity().setTitle(cat);
+
+        }else if(cat.equals("Foodie")){
+
+
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
+        }else if(cat.equals("Foodie")){
+
+
         }
+
+
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onListItemClick(ListView l, View v, int pos, long id) {
+        super.onListItemClick(l, v, pos, id);
+        Intent intent = new Intent(getActivity(), FinalActivity.class);
+        startActivity(intent);
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
+
+
 }
